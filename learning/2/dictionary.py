50f57ca4a54ed21f97ede54d88fb1ebac06645bc
Your Name <you@example.com>
2025-11-06 14:44:53 +0100

    sup

---
+++ learning/2/dictionary.py
@@ -0,0 +1,69 @@
+print()
+
+# * Dictionary is a data type used to store data values in key-value pairs
+# ? variable = {key:information, key:information}
+
+# person = {"forname":"Buddy", "lastname":"Boy"}
+
+# print(person)
+# # To get info from a dictionary, you have to get the key first
+# print(f"The information to the key (forname) is: {person['forname']}") 
+
+# # ? Change the key
+# person["lastname"] = "The Great"
+# print(person)
+
+# # ? Add keys into the dictionary
+# person["birth-year"] = 1995
+# print(person)
+
+
+# # ? Delete keys in the dictionary (del)
+# del person["birth-year"]
+# print(person)
+
+
+# 2) With built-in loops (keys(), value(), )
+
+# car = {"Logo":"VW", "Model":"ID.4", "Km_stand":5000}
+
+# # ? Write out the keys with the use of keys()
+# print(car.keys())
+# for key in car.keys():
+#     print(f"{key:^2}")
+
+# print()
+# # ? Write out values with the use of values()
+# print(car.values())
+# for value in car.values():
+#     print(value)
+
+# print()
+# # ? Write out the elements with the use of items()
+# print(car.items())
+# for items in car.items():
+#     print(items)
+
+# print()
+# # ? Write out list of info in the dictionary
+
+# # 1 method. Use key()
+# for key in car.keys():
+#     print(f"{key:<10}:{car[key]:>8}")
+# print()
+
+# # 2 method. use items()
+# for key, value in car.items():
+#     print(f"{key:<10}:{value:>8}")
+
+
+car_list = [{"Logo":"VW", "Model":"ID.4", "Km_stand":5000},
+            {"Logo":"BMW", "Model":"ID.1", "Km_stand":100000},
+            {"Logo":"Porche", "Model":"ID.14", "Km_stand":2500}] # ? You can create lists of dictionarys
+
+for car in car_list:
+    print(car)
+
+
+
+
--- tasks/2/global_variables_task.py
+++ tasks/2/global_variables_task.py
@@ -1,9 +1,23 @@
 print()
 def christmas_tree(size: str) -> None:
-    if size == "small":
-        for i in range(0,12):
-            if i % 3 == 0:
-                print(i * "*")
-                print()
-                
-christmas_tree("small")
+    total_rows = 0
+    
+    if size == "large":
+        total_rows = 7
+    elif size == "medium":
+        total_rows = 5
+    elif size == "small":
+        total_rows = 3
+    else:
+        print("Write either small, medium or large to generate your christmas tree")
+        return
+        
+    for i in range(total_rows):
+        print(f"{"*" * (2*i+1):^20}")
+    print()
+
+
+size_variable = str(input("Write small, medium or large to create your christmas tree: "))
+print()
+
+christmas_tree(size_variable)
