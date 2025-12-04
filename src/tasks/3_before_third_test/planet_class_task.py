048d85eeab216b7c63c4a1915e65267c589f2c09
Your Name <you@example.com>
2025-12-05 00:31:43 +0100

    commit #21 OOP tasks

--- src/tasks/3_before_third_test/planet_class_task.py
+++ src/tasks/3_before_third_test/planet_class_task.py
@@ -2,113 +2,86 @@ import math as m
 
 print()
 class Planet:
-    """
-    Docstring for Planet
     
-    Class to create Planet objects
-    """
-    
-    def __init__(self, name: str, diameter: float, distance_from_sun: float, total_moons: int = 0):
-        """
-        Docstring for __init__
-        
-        :param self: Description
-        :param name: Description
-        :type name: str
-        :param diameter: Description
-        :type diameter: float
-        :param distance_from_sun: Description
-        :type distance_from_sun: float
-        :param total_moons: Description
-        :type total_moons: int
-        
-        Declares objects as the parameters
-        """
-        
-        
+    def __init__(self, name: str, diameter: float, distance_from_sun: float, moon_list = None) -> None:
+
+        if moon_list is None:
+            moon_list = []
+
         self.name = name
         self.diameter = diameter
         self.distance_from_sun = distance_from_sun
-        self.total_moons = total_moons
+        self.total_moons = len(moon_list)
+        self.moon_list = moon_list
         
+    def calculate_total_moons(self) -> int:
+        return len(self.moon_list)
         
-    def calculate_volume(self):
-        
-        """
-        Docstring for calculate_volume
-        
-        Calculates the volume of the planet
-        """
+    def calculate_planet_volume(self) -> float:
         
         radius = self.diameter / 2
         return round(4/3 * m.pi * radius**3, 2)
     
-    def calculate_surface_area(self):
-        
-        """
-        Docstring for calculate_surface_area
-        
-        :param self: Description
-        """
-        
+    def calculate_surface_area(self) -> float:
         
         radius = self.diameter / 2
         return round(4 * m.pi * radius**2, 2)
     
-    def show_info(self):
+    def show_info(self) -> None:
         
-        """
-        Docstring for show_info
+        print(f"Planet Name:         | {self.name:^25}")
+        print(f"Planet Diameter:     | {self.diameter:^25} km") 
+        print(f"Distance from Sun:   | {self.distance_from_sun:^25} million km") 
+        print(f"Total Moons:         | {self.calculate_total_moons():^25} Moons")
+        print(f"Planet Volume:       | {round(self.calculate_planet_volume(), 2):^25} km^3")
+        print(f"Planet Surface Area: | {round(self.calculate_surface_area(), 2):^25} km^2")
+        print()
+        print(f"{self.name} Moons:")
+        print("-" * 80)
         
-        :param self: Description
-        """
+        if not self.moon_list:
+            print("This Planet has no Moons")
+            
+        else:
+            for moon in self.moon_list:
+                print(f"Moon Name: {moon.name}")
+                print(f"Moon Volume: {round(moon.calculate_moon_volume(), 2)} km^3")
+                print(f"Earth-Volume Ratio: {round(moon.calculate_moon_volume() / self.calculate_planet_volume(), 2)} km^3")
+                print()
+
+class Moon:
+    
+    def __init__(self, name: str, moon_diameter: float) -> None:
         
-        print(f"{self.name:^25}|{self.diameter:^25}|{self.distance_from_sun:^25}|{self.total_moons:^25}|{round(self.calculate_volume(), 2):^25}|{round(self.calculate_surface_area(), 2):^25}|")
+        self.name = name
+        self.moon_diameter = moon_diameter
+        
+    def calculate_moon_volume(self) -> float:
+        radius = self.moon_diameter / 2
+        return round(4/3 * m.pi * radius**3, 2)
+    
 
-# Mercury = Planet("Mercury", 4879, 579.9)
-# Venus = Planet("Venus", 12104, 108.21)
-# Earth = Planet("Earth", 12742, 149.60, 1)
-# Mars = Planet("Mars", 6779, 227.923, 2)
-# Jupiter = Planet("Jupiter", 139822, 778.570, 2)
-# Saturn = Planet("Saturn", 120536, 1433.530, 2)
-# Uranus = Planet("Uranus", 50724, 2872.460, 2)
-# Neptune = Planet("Neptune", 49244, 4495.06, 2)
-
-
-# print(f"{"Planet":^25}|{"Diameter (km)":^25}|{"Sun Distance (million km)":^25}|{"Total Moons":^25}|{"Volume (km^3)":^25}|{"Surface Area (km^2)":^25}|")
-# print("---" * 53)
-
-# planet_list = [Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune]
-
-# for planet in planet_list:
-#     planet.show_info()
-
-
-planets = {}
-
-planets["Mercury"] = Planet("Mercury", 4879, 579.9)
-planets["Venus"] = Planet("Venus", 12104, 108.21)
-planets["Earth"] = Planet("Earth", 12742, 149.60, 1)
-planets["Mars"] = Planet("Mars", 6779, 227.923, 2)
-planets["Jupiter"] = Planet("Jupiter", 139822, 778.570, 2)
-planets["Saturn"] = Planet("Saturn", 120536, 1433.530, 2)
-planets["Uranus"] = Planet("Uranus", 50724, 2872.460, 2)
-planets["Neptune"] = Planet("Neptune", 49244, 4495.06, 2)
+planets: dict = {
+    "Mercury": Planet("Mercury", 4879, 579.9),
+    "Venus": Planet("Venus", 12104, 108.21),
+    "Earth": Planet("Earth", 12742, 149.60, [Moon("Luna", 3474.8)]),
+    "Mars": Planet("Mars", 6779, 227.923, [Moon("Phobos", 22.2), Moon("Deimos", 12.4)]),
+    "Jupiter": Planet("Jupiter", 139822, 778.570),
+    "Saturn": Planet("Saturn", 120536, 1433.530),
+    "Uranus": Planet("Uranus", 50724, 2872.460),
+    "Neptune": Planet("Neptune", 49244, 4495.06)
+}
 
 
 try:
-    choose_planet = str(input("Enter a planet in the solar system for details: "))
+    choose_planet = str(input("Enter a planet in the solar system for details: ")).capitalize()
     
-    for planet_key, planet_info in planets.items():
-        if choose_planet == planet_key:
-            print()
-            print(f"{"Planet":^25}|{"Diameter (km)":^25}|{"Sun Distance (million km)":^25}|{"Total Moons":^25}|{"Volume (km^3)":^25}|{"Surface Area (km^2)":^25}|")
-            print("---" * 53)
-            planet_info.show_info()
-            
-except Exception as e:
-    print(f"INVALID NAME!")
-    print(f"System message: {e}")
-    print()
-    choose_planet = str(input("Enter a planet in the solar system for details: "))
+    if choose_planet in planets:
+        print()
+        planets[choose_planet].show_info()
+    else:
+        print("\nINVALID NAME!")
 
+except Exception as e:
+    print("\nERROR OCCURRED!")
+    print(f"System message: {e}")
